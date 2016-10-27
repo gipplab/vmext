78f435ebd81a84104fdbec4f66126b987b750360
Petulant Slacker <l.goohsen@gmail.com>
2016-10-27 19:56:31 +0200

    extracted xml-parsing into module

--- api/controller/v1/abstractSyntaxTreeController.js
+++ api/controller/v1/abstractSyntaxTreeController.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const path = require('path'),
+      xmlParser = require(path.join(process.cwd(), 'lib', 'xmlParser')),
       BadRequestError = require(path.join(process.cwd(), 'errorHandler', 'BadRequestError')),
       ConflictError = require(path.join(process.cwd(), 'errorHandler', 'ConflictError'));
 
 module.exports = class AbstractSyntaxTreeController {
   static renderAst(req, res, next) {
     if (!req.body) return next(new BadRequestError("request body is empty or not application/json encoded!"));
-    console.log(req.query);
-    res.send(req.body);
+    xmlParser.parseXML(req.body.mathml, (err, result) => {
+      if (err) return next(new ConflictError('XML- Parsing Error'));
+      res.json(result);
+    });
   }
 };
