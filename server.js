15903239b6b77e964e77a1cc0338348c45699f5a
Petulant Slacker <l.goohsen@gmail.com>
2016-11-07 16:05:51 +0100

    added demo index page

--- server.js
+++ server.js
@@ -5,17 +5,26 @@ const express = require('express'),
       compression = require('compression'),
       bodyParser = require('body-parser'),
       winston = require('winston'),
-      logger = require('./lib/logger'),
-      log_general = winston.loggers.get('general');
+      //logger = require('./lib/logger'),
+      log_general = winston.loggers.get('general'),
+
+      SVGRenderer = require('./lib/SVGRenderer');
 
 // compress all assets and json-responses if minimal size is reached
 app.use(compression());
 
 app.use(bodyParser.urlencoded({extended: false})); // url-form encoded data
 
+app.set('view engine', 'pug');
+app.set('views', './public/html');
+
 // serve static content only from 'public' dir
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', require('./api/versions'));
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
 
 // global errorHandler ============================================
 require(path.join(__dirname, 'errorHandler','ErrorHandler'))(app);
