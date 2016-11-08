241c99c02238f9c7f2672611cf3d90d3106c1b99
Petulant Slacker <l.goohsen@gmail.com>
2016-11-08 15:43:13 +0100

    added capistrano, upstart config

---
+++ config/deploy.rb
@@ -0,0 +1,49 @@
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+set :application, 'htw-citeplag-node'
+set :repo_url, 'git@bitbucket.org:LuiSlacker/htw-citeplag-node.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/var/www/pds-node'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+  task :build_and_restart do
+    on roles :app do
+      within current_path do
+        puts 'Installing node modules...'
+        execute :npm, 'install --no-optional'
+      end
+    end
+  end
+
+  after :deploy, :build_and_restart
+end
