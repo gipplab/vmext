f207f3ba1e38b2fb58ddd9d303e6606d615b63f3
Petulant Slacker <l.goohsen@gmail.com>
2016-11-29 17:40:25 +0100

    refactored errorHandler

--- errorHandler/ErrorHandler.js
+++ errorHandler/ErrorHandler.js
@@ -1,6 +1,6 @@
-const path = require('path');
+'use strict';
 const log = require('../lib/logger');
-const NotFoundError = require(path.join(__dirname, 'NotFoundError.js'));
+const NotFoundError = require('./NotFoundError.js');
 
 module.exports = (app) => {
   // catch 404s
@@ -8,28 +8,15 @@ module.exports = (app) => {
     next(new NotFoundError(`Endpoint ${req.originalUrl} for method ${req.method} is not defined.`));
   });
 
-  // production errors: no stackTraces leaked to user
-  if (app.get('env') === 'production') {
-    app.use((err, req, res, next) => {
-      const error = {
+  app.use((err, req, res, next) => {
+    log.error(err);
+    res.status(err.status).json({
+      Error: {
         name: err.name,
+        status: err.status,
         message: err.message,
-        status: err.status || 500
-      };
-      log.error(error);
-      res.status(error.status).json({ error });
+        stack: app.get('env') === 'production' ? null : err.stack
+      }
     });
-  } else {
-    // development errors: print stacktrace
-    app.use((err, req, res, next) => {
-      const error = {
-        name: err.name,
-        message: err.message,
-        status: err.status || 500,
-        stack: err.stack
-      };
-      log.error(error);
-      res.status(error.status).json({ error });
-    });
-  }
+  });
 };
