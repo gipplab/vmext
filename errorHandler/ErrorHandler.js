a2007be0b1936f7b91959f993cdc6b31b379ddbb
Petulant Slacker <l.goohsen@gmail.com>
2017-01-26 18:21:08 +0100

    improved errorHandler

--- errorHandler/ErrorHandler.js
+++ errorHandler/ErrorHandler.js
@@ -11,6 +11,7 @@ module.exports = (app) => {
 
   app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
     log.error(err);
-    res.status(err.status || 500).json({ Error: err });
+    const BoomError = !err.isBoom ? Boom.wrap(err): err;
+    res.status(err.status || 500).json({ Error: BoomError });
   });
 };
