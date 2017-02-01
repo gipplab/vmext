a7f3a21d8f2816b749468dc06cf77f59ab5e35ea
Petulant Slacker <l.goohsen@gmail.com>
2017-02-01 16:52:37 +0100

    refactored simplaAST-wiget to use iframe

--- public/widgets/formula-ast-widget.js
+++ public/widgets/formula-ast-widget.js
@@ -1,112 +1,27 @@
-// iife - to encapsulate scope
-(function(){
+'use strict';
 
-  // ENUM declarations
-  var Dimension = {
-    WIDTH: 'width',
-    HEIGHT: 'height',
+(function() { // iife - to encapsulate scope
+  const script = document.currentScript;
+  const attributes = {
+    mathml: script.getAttribute('mathml'),
+    collapseSingleOperandNodes: script.getAttribute('collapseSingleOperandNodes'),
+    nodesToBeCollapsed: script.getAttribute('nodesToBeCollapsed'),
   }
+  const queryParams = encodeQueryParams(attributes);
 
-  var defaults = {
-    minNodeSize: 30,
-    exScalingFactor: 9,
-    nodeHoverScaling: 1.2,
-    color: {
-      referenceNode: '#EDF1FA',
-      referenceNodeHighlight: '#d5e1fd',
-      comparisonNode: '#edfaf1',
-      comparisonNodeHighlight: '#d6f5e0',
-      singleAST: '#FFF',
-    }
+  //iframe element
+  const iframe = document.createElement('iframe');
+  iframe.src = `http://localhost:4001/widgets/formula-ast/index.html?${queryParams}`;
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+
+  script.parentNode.replaceChild(iframe, script);
+
+  function encodeQueryParams({ mathml, collapseSingleOperandNodes, nodesToBeCollapsed }) {
+    const queryParams = new URLSearchParams();
+    queryParams.append('mathml', mathml);
+    queryParams.append('collapseSingleOperandNodes', collapseSingleOperandNodes);
+    queryParams.append('nodesToBeCollapsed', nodesToBeCollapsed);
+    return queryParams.toString();
   }
-
-  var script = document.currentScript;
-  loadVendorScripts(script); // fetch cytoscape and dagre-layout bundle
-
-  function loadVendorScripts(script) {
-    var scriptTag = document.createElement('script');
-    scriptTag.setAttribute('type', 'text/javascript');
-    scriptTag.setAttribute('src', 'http://math.citeplag.org/widgets/cytoscape-dagre-bundle.min.js');
-    scriptTag.onload = onVendorScriptsLoaded.bind(this, script);
-    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(scriptTag);
-  }
-
-  function onVendorScriptsLoaded(script) {
-    var container = document.createElement('div');
-    container.style.width = '100%';
-    container.style.height = '100%';
-    container.classList.add('cy-container');
-
-    var gifLoader = document.createElement('img');
-    gifLoader.classList.add('gif-loader');
-    gifLoader.style.position = 'absolute';
-    gifLoader.style.top = '50%';
-    gifLoader.style.left = '50%';
-    gifLoader.style['margin-top'] = '-79px';
-    gifLoader.style['margin-left'] = '-79px';
-    gifLoader.src = 'http://math.citeplag.org/assets/ring.gif';
-
-    container.appendChild(gifLoader);
-    script.parentNode.replaceChild(container, script);
-    var formData = new FormData();
-    formData.append('mathml', script.getAttribute('mathml'));
-    formData.append('collapseSingleOperandNodes', script.getAttribute('collapseSingleOperandNodes'));
-    formData.append('nodesToBeCollapsed', script.getAttribute('nodesToBeCollapsed'));
-    fetch('http://math.citeplag.org/api/v1/math/renderAST?cytoscaped=true', {
-      method: 'POST',
-      headers: new Headers({
-        'Accept': 'application/json',
-      }),
-      body: formData
-    }).then(function(data) {
-      return data.json();
-    }).then(function(result) {
-      renderAST(result);
-      document.querySelector('.gif-loader').style.display = 'none';
-    }).catch(function(err) {
-      console.error(err);
-    });
-  }
-
-  function renderAST(elements) {
-    var referenceAST =  cytoscape({
-      container: document.querySelector('.cy-container'),
-      elements: elements,
-      style: [
-        {
-          selector: '.source-A',
-          css: {
-            shape: 'roundrectangle',
-            'background-color': 'white',
-            'background-image': 'data(presentation)',
-            'background-fit': 'none',
-            width: function(ele) {
-              return extractDimensionsFromSVG(ele, Dimension.WIDTH);
-            },
-            height: function(ele) {
-              return extractDimensionsFromSVG(ele, Dimension.HEIGHT);
-            },
-            'border-width': '2px',
-            'border-color': 'steelblue'
-          }
-        },
-        {
-          selector: 'edge',
-          css: {
-            'line-color': '#ccc'
-          }
-        }
-      ],
-      layout: {
-        name: 'dagre',
-      }
-    });
-  }
-
-  function extractDimensionsFromSVG(ele, type) {
-    var dimensionInEX = ele.data().presentation.match(`${type}%3D%22([0-9]*.[0-9]*)ex`)[1];
-    var dimensioninPX = dimensionInEX * defaults.exScalingFactor;
-    return dimensioninPX > defaults.minNodeSize ? dimensioninPX : defaults.minNodeSize;
-  }
-
-})(); // iife end
+})();
---
+++ public/widgets/formula-ast/renderAST.js
@@ -0,0 +1,92 @@
+// ENUM declarations
+const Dimension = {
+  WIDTH: 'width',
+  HEIGHT: 'height',
+}
+
+const defaults = {
+  minNodeSize: 30,
+  exScalingFactor: 9,
+  nodeHoverScaling: 1.2,
+  color: {
+    referenceNode: '#EDF1FA',
+    referenceNodeHighlight: '#d5e1fd',
+    comparisonNode: '#edfaf1',
+    comparisonNodeHighlight: '#d6f5e0',
+    singleAST: '#FFF',
+  }
+};
+
+const queryParams = extractQueryParams();
+fetchData(queryParams)
+  .then((result) => {
+    renderAST(result);
+    document.querySelector('.gif-loader').style.display = 'none';
+  })
+  .catch(console.error)
+
+function extractQueryParams() {
+  const queryParams = new URLSearchParams(window.location.search);
+  const mathml = queryParams.get('mathml')
+  const collapseSingleOperandNodes = queryParams.get('collapseSingleOperandNodes')
+  const nodesToBeCollapsed = queryParams.get('nodesToBeCollapsed')
+  return {
+    mathml,
+    collapseSingleOperandNodes,
+    nodesToBeCollapsed,
+  }
+}
+
+function fetchData({ mathml, collapseSingleOperandNodes, nodesToBeCollapsed }) {
+  const formData = new FormData();
+  formData.append('mathml', mathml);
+  formData.append('collapseSingleOperandNodes', collapseSingleOperandNodes);
+  formData.append('nodesToBeCollapsed', nodesToBeCollapsed);
+  return fetch('http://math.citeplag.org/api/v1/math/renderAST?cytoscaped=true', {
+    method: 'POST',
+    headers: new Headers({
+      'Accept': 'application/json',
+      'referer': 'vbb',
+    }),
+    body: formData
+  }).then((data) => {
+    return data.json();
+  });
+}
+
+function renderAST(elements) {
+  const formulaAST =  cytoscape({
+    container: document.querySelector('.cy-container'),
+    elements: elements,
+    style: [
+      {
+        selector: '.source-A',
+        css: {
+          shape: 'roundrectangle',
+          'background-color': 'white',
+          'background-image': 'data(presentation)',
+          'background-fit': 'none',
+          width: (ele) => extractDimensionsFromSVG(ele, Dimension.WIDTH),
+          height: (ele) => extractDimensionsFromSVG(ele, Dimension.HEIGHT),
+          'border-width': '2px',
+          'border-color': 'steelblue'
+        }
+      },
+      {
+        selector: 'edge',
+        css: {
+          'line-color': '#ccc'
+        }
+      }
+    ],
+    layout: {
+      name: 'dagre',
+    }
+  });
+}
+
+function extractDimensionsFromSVG(ele, type) {
+  const dimensionInEX = ele.data().presentation.match(`${type}%3D%22([0-9]*.[0-9]*)ex`)[1];
+  const dimensioninPX = dimensionInEX * defaults.exScalingFactor;
+  return dimensioninPX > defaults.minNodeSize ? dimensioninPX : defaults.minNodeSize;
+}
