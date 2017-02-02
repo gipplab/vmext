e78dd2d08302acf874ede3ffbff30cad06c75b1e
Petulant Slacker <l.goohsen@gmail.com>
2017-02-02 12:20:15 +0100

    readded mouseover events in mergedAST

--- public/widgets/formula-similarity/render-similarities.js
+++ public/widgets/formula-similarity/render-similarities.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const Origin = {
+  REFERENCE_AST: 0,
+  COMPARISON_AST: 1,
+};
+let mergedAST;
+let referenceAST;
+let comparisonAST;
+
 window.addEventListener('message', paramsReveived, false);
 
 function paramsReveived(event) {
@@ -7,6 +15,7 @@ function paramsReveived(event) {
   fetchData(attributes)
     .then((result) => {
       renderAST(result);
+      attachEventListeners();
       document.querySelector('.gif-loader').style.display = 'none';
       document.querySelector('.reference-ast-container').style['background-color']= '#EDF1FA';
       document.querySelector('.comparison-ast-container').style['background-color']= '#edfaf1';
@@ -27,7 +36,7 @@ function fetchData({ reference_mathml, comparison_mathml, similarities }) {
   return fetch('http://math.citeplag.org/api/v1/math/renderMergedAST', {
     method: 'POST',
     headers: new Headers({
-      'Accept': 'application/json',
+      Accept: 'application/json',
     }),
     body: formData,
     referrerPolicy: 'no-referrer',
@@ -41,8 +50,8 @@ function fetchData({ reference_mathml, comparison_mathml, similarities }) {
   });
 }
 
-function renderAST({cytoscapedMergedAST, cytoscapedReferenceAST, cytoscapedComparisonAST}) {
-  const mergedAST = cytoscape({
+function renderAST({ cytoscapedMergedAST, cytoscapedReferenceAST, cytoscapedComparisonAST }) {
+  mergedAST = cytoscape({
     container: document.querySelector('.merged-ast-container'),
     elements: cytoscapedMergedAST,
     style: [
@@ -113,7 +122,7 @@ function renderAST({cytoscapedMergedAST, cytoscapedReferenceAST, cytoscapedCompa
       directed: true
     }
   });
-  const referenceAST =  cytoscape({
+  referenceAST =  cytoscape({
     container: document.querySelector('.reference-ast-container'),
     elements: cytoscapedReferenceAST,
     style: [
@@ -141,7 +150,7 @@ function renderAST({cytoscapedMergedAST, cytoscapedReferenceAST, cytoscapedCompa
       name: 'dagre',
     }
   });
-  const comparisonAST =  cytoscape({
+  comparisonAST =  cytoscape({
     container: document.querySelector('.comparison-ast-container'),
     elements: cytoscapedComparisonAST,
     style: [
@@ -176,4 +185,120 @@ function extractDimensionsFromSVG(ele, type) {
   const dimensionInEX = ele.data().presentation.match(`${type}%3D%22([0-9]*.[0-9]*)ex`)[1];
   const dimensioninPX = dimensionInEX * defaults.exScalingFactor;
   return dimensioninPX > defaults.minNodeSize ? dimensioninPX : defaults.minNodeSize;
+}
+
+/*
+**  event listeners
+*/
+
+function attachEventListeners() {
+  referenceAST.on('mouseover', 'node', function(event) {
+    const nodeID = event.cyTarget.id();
+    const node = referenceAST.$(`node[id='${nodeID}']`);
+    const mouseOverNode = new CustomEvent('mouseOverNode',
+    {
+      detail: {
+        origin: Origin.REFERENCE_AST,
+        nodeID,
+      }
+    });
+    window.dispatchEvent(mouseOverNode);
+    highlightNode(node);
+  });
+
+  referenceAST.on('mouseout', 'node', function(event) {
+    const nodeID = event.cyTarget.id();
+    const node = referenceAST.$(`node[id='${nodeID}']`);
+    const mouseOutNode = new CustomEvent('mouseOutNode',
+    {
+      detail: {
+        origin: Origin.REFERENCE_AST,
+        nodeID,
+      }
+    });
+    window.dispatchEvent(mouseOutNode);
+    unhighlightNode(node);
+  });
+
+  comparisonAST.on('mouseover', 'node', function(event) {
+    const nodeID = event.cyTarget.id();
+    const node = comparisonAST.$(`node[id='${nodeID}']`);
+    const mouseOverNode = new CustomEvent('mouseOverNode',
+    {
+      detail: {
+        origin: Origin.COMPARISON_AST,
+        nodeID: event.cyTarget.id(),
+      }
+    });
+    window.dispatchEvent(mouseOverNode);
+    highlightNode(node);
+  });
+
+  comparisonAST.on('mouseout', 'node', function(event) {
+    const nodeID = event.cyTarget.id();
+    const node = comparisonAST.$(`node[id='${nodeID}']`);
+    const mouseOutNode = new CustomEvent('mouseOutNode',
+    {
+      detail: {
+        origin: Origin.COMPARISON_AST,
+        nodeID,
+      }
+    });
+    window.dispatchEvent(mouseOutNode);
+    unhighlightNode(node);
+  });
+
+
+  window.addEventListener('mouseOverNode', (event) => {
+    const node = mergedAST.$(`node[id='${event.detail.nodeID}']`);
+    const origin  = event.detail.origin;
+    const color = (origin === Origin.REFERENCE_AST)
+     ? defaults.color.referenceNodeHighlight
+     : defaults.color.comparisonNodeHighlight;
+    highlightNode(node);
+  });
+
+  window.addEventListener('mouseOutNode', (event) => {
+    const node = mergedAST.$(`node[id='${event.detail.nodeID}']`);
+    const origin  = event.detail.origin;
+    const color = (origin === Origin.REFERENCE_AST)
+     ? defaults.color.referenceNode
+     : defaults.color.comparisonNode;
+    unhighlightNode(node);
+  });
+}
+
+/*
+** Animation Helper
+*/
+function highlightNode(node) {
+  const newWidth = Math.floor(node.style('width').match('([0-9]*.[0-9]*)px')[1]) * defaults.nodeHoverScaling;
+  const newHeight = Math.floor(node.style('height').match('([0-9]*.[0-9]*)px')[1]) * defaults.nodeHoverScaling;
+  node.data('oldWidth', node.style('width'));
+  node.data('oldHeight', node.style('height'));
+  node.animate(
+    {
+      css: {
+        width: newWidth,
+        height: newHeight
+      }
+    },
+    {
+      duration: 100
+    }
+  );
+}
+
+function unhighlightNode(node) {
+  node.animate(
+    {
+      css: {
+        width: node.data('oldWidth'),
+        height: node.data('oldHeight')
+      }
+    },
+    {
+      duration: 100
+    }
+  );
 }
