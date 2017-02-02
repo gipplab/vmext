f65fc45583f1ae4b3eeb64e9ee7ee59d293afa3d
Petulant Slacker <l.goohsen@gmail.com>
2017-02-02 12:23:35 +0100

    fixed hostname

--- public/widgets/formula-similarity-widget.js
+++ public/widgets/formula-similarity-widget.js
@@ -11,7 +11,7 @@
   // iframe element
   const iframe = document.createElement('iframe');
   iframe.classList.add('formel-similarities-iframe');
-  iframe.src = 'http://localhost:4001/widgets/formula-similarity/index.html';
+  iframe.src = 'http://math.citeplag.org/widgets/formula-similarity/index.html';
   iframe.style.width = '100%';
   iframe.style.height = '100%';
   iframe.onload = () => {
