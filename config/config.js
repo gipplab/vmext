4121d2ef934508494ff70bdee35b416fc3a36120
Petulant Slacker <l.goohsen@gmail.com>
2017-03-16 15:49:52 +0100

    removed hardcoded port in config

--- config/config.js
+++ config/config.js
@@ -1,9 +1,7 @@
 'use strict';
 
 module.exports = {
-  base_remote_url: 'http://math.citeplag.org/',
-  base_local_url: 'http://localhost/4001',
-  host: (process.env.NODE_ENV === 'production') ? 'math.citeplag.org' : 'localhost:4001',
+  host: (process.env.NODE_ENV === 'production') ? 'math.citeplag.org' : `localhost:${process.env.PORT || '4001'}`,
   slack: {
     webhook: 'https://hooks.slack.com/services/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',
     channels: {
