7c3c0f19ad39ad0aded8b16c3143f8015ddbcfed
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2017-11-24 23:19:30 +0100

    Use special config for travis

--- config/config.js
+++ config/config.js
@@ -2,4 +2,7 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 module.exports = yaml.safeLoad(fs.readFileSync(__dirname + '/../config.yaml'));
+if(process.env.TRAVIS){
+  module.exports.mathoidUrl = 'http://localhost:10044';
+}
 
