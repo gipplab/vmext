c15ffd0fe2e3919bb361717c4d5d993a0939f0fb
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2017-11-26 08:27:35 +0100

    Add basic api test

---
+++ test/api/basic.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+describe('api test', () => {
+  let server;
+  beforeEach(() => {
+    server = require('../../server').start();
+  });
+  afterEach(() => {
+    server.close();
+  });
+  const endpoints = ['parseAST','renderPMML', 'renderMergedAST' ,'parseCytoscapedMergedAst', 'parseCytoscapedAST','renderAST'];
+  endpoints.forEach((t) => {
+    // TODO: broken test
+    it('handle empty requests ' + t, function testSlash(done) {
+      request(server)
+        .post(`/api/v1/math/${t}`)
+        .expect(500, done);
+    });
+  });
+
+});
