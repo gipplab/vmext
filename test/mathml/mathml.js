25e96672afd74d9d17acdd8125e03db77e950cd4
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2018-03-13 17:57:32 +0100

    inherit the xtraverse api completely

--- test/mathml/mathml.js
+++ test/mathml/mathml.js
@@ -10,21 +10,23 @@ describe('MathML reading', () => {
   const xmlString = fs.readFileSync(file, 'utf8');
   const pFile = path.resolve('data/10-pmml-annotation.mml.xml');
   const xmlPString = fs.readFileSync(pFile, 'utf8');
-  it('should fail with empty input',() => assert.throws(() => new MathML()));
-  it('should initialize with goat input',() => new MathML(xmlString));
+  it('should initialize with goat input',() => MathML(xmlString));
   it('should get goat content',() => {
-    const mathml = new MathML(xmlString);
-    const contentRoot = mathml.contentRoot;
+    const mathml = MathML(xmlString);
+    const contentRoot = mathml.contentRoot();
     assert(contentRoot);
     assert.equal(contentRoot.name(),'apply');
-    assert.equal(contentRoot.attr('id'),'e40');
+    assert.equal(contentRoot.id(),'e40');
   });
-  it('should initialize with pmml annotations',() => new MathML(xmlPString));
+  it('should initialize with pmml annotations',() => MathML(xmlPString));
   it('should get pmml annotations content',() => {
-    const mathml = new MathML(xmlPString);
-    const contentRoot = mathml.contentRoot;
+    const mathml = MathML(xmlPString);
+    const contentRoot = mathml.contentRoot();
     assert(contentRoot);
     assert.equal(contentRoot.name(),'apply');
-    assert.equal(contentRoot.attr('id'),'w2');
+    assert.equal(contentRoot.id(),'w2');
+    assert.equal(contentRoot.xref(),'w29');
+    assert(contentRoot.contentRoot());
   });
+  it('should fail for empty input',() => assert.throws(() => MathML().contentRoot()));
 });
