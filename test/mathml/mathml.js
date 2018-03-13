033f3629ef18d76f39b36a632f1324813f172bee
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2018-03-13 15:53:23 +0100

    Improve test coverage and API coherence

--- test/mathml/mathml.js
+++ test/mathml/mathml.js
@@ -6,14 +6,25 @@ const path = require('path');
 const assert = require('assert');
 
 describe('MathML reading', () => {
-  const file = path.resolve('data/10-pmml-annotation.mml.xml');
+  const file = path.resolve('data/09-goat.mml.xml');
   const xmlString = fs.readFileSync(file, 'utf8');
+  const pFile = path.resolve('data/10-pmml-annotation.mml.xml');
+  const xmlPString = fs.readFileSync(pFile, 'utf8');
   it('should fail with empty input',() => assert.throws(() => new MathML()));
   it('should initialize with goat input',() => new MathML(xmlString));
   it('should get goat content',() => {
     const mathml = new MathML(xmlString);
     const contentRoot = mathml.contentRoot;
     assert(contentRoot);
-    assert.equal(contentRoot.nodeName,'semantics');
+    assert.equal(contentRoot.name(),'apply');
+    assert.equal(contentRoot.attr('id'),'e40');
+  });
+  it('should initialize with pmml annotations',() => new MathML(xmlPString));
+  it('should get pmml annotations content',() => {
+    const mathml = new MathML(xmlPString);
+    const contentRoot = mathml.contentRoot;
+    assert(contentRoot);
+    assert.equal(contentRoot.name(),'apply');
+    assert.equal(contentRoot.attr('id'),'w2');
   });
 });
