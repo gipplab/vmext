d394863825389e1f2ebd7dd6ed70aefd38906aa2
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2018-03-14 16:06:32 +0100

    Add cytoscape tree generator

---
+++ test/mathml/cytoscape.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const cytoscapeRenderer = require('../../lib/MathML/cytoscape');
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+
+const file = path.resolve('data/09-goat.mml.xml');
+const xmlString = fs.readFileSync(file, 'utf8');
+
+describe('cytoscape rendering', () => {
+
+  it('should initialize with goat input', () => cytoscapeRenderer(xmlString));
+  it('should get correct number of nodes in goat tree', () => {
+    const mathml = cytoscapeRenderer(xmlString);
+    const cy = mathml.toCytoscape();
+    assert(cy);
+    assert.equal(cy.elements().length,39);
+    assert.equal(cy.edges().length,19);
+    assert.equal(cy.nodes().length,20);
+  });
+});
