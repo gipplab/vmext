3c50c773816e6ad7b540cf3dc4bb6106073773a7
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2018-04-10 17:39:39 +0200

    Use client rendering for cytoscape

--- frontend/js/ast-renderer.js
+++ frontend/js/ast-renderer.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let buffers = {};
-let elem = document.getElementById('MathMLexamples');
-let formats = {cmml:{},pmml:{}};
+const buffers = {};
+const elem = document.getElementById('MathMLexamples');
+const formats = { cmml:{},pmml:{} };
 
 
 /**
@@ -15,7 +15,7 @@ function selectBuffer(editor, name) {
   if (buf.getEditor()) {
     buf = buf.linkedDoc({ sharedHist: true });
   }
-  let old = editor.swapDoc(buf);
+  const old = editor.swapDoc(buf);
   let linked;
   old.iterLinkedDocs(doc => linked = doc);
   if (linked) {
@@ -39,22 +39,21 @@ function callAPI(evt) {
   if (evt) {
     evt.preventDefault();
   }
-  Object.keys(formats).forEach((f)=>{
+  Object.keys(formats).forEach((f) => {
     selectBuffer(formats[f].cm, elem.options[elem.selectedIndex].label);
   });
-  const scriptTag = document.createElement('script');
-  scriptTag.setAttribute('type', 'application/javascript');
-  scriptTag.setAttribute('src', '/widgets/formula-ast-widget.js');
-  scriptTag.setAttribute('mathml', formats.cmml.cm.getValue());
-
-  const container = document.querySelector('.abstract-syntax-tree');
-  container.innerHTML = "";
-  container.appendChild(scriptTag);
+  const mmlCy = require('../../lib/MathML/cytoscape');
+  const container = document.getElementById('mast');
+  const mml = mmlCy.mml(formats.cmml.cm.getValue());
+  mml.toCytoscape({ container,
+    boxSelectionEnabled: false,
+    autounselectify: true,
+    applyForm: true
+  });
 }
 
 window.onload = function init() {
-
-  Object.keys(formats).forEach((f)=>{
+  Object.keys(formats).forEach((f) => {
     const mml = document.getElementById(f);
     formats[f].cm = CodeMirror(mml, { lineNumbers: true });
   });
@@ -64,11 +63,10 @@ window.onload = function init() {
     o => buffers[o.label] = CodeMirror.Doc(o.value, 'application/xml')
   );
 
-  callAPI();
-
-  elem.addEventListener('change', function(){
+  elem.addEventListener('change', () => {
     callAPI();
   });
+  callAPI();
   window.formats = formats;
 };
 
@@ -77,3 +75,7 @@ const renderPNG = () => {
   document.querySelector('.btn-download').href = canvas.toDataURL('image/png').replace(/^data:image\/[^;]/, 'data:application/octet-stream');
 };
 
+document.addEventListener('DOMContentLoaded', () => {
+
+
+});
