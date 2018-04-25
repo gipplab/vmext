979a5849bc47a2fbf3f37015c392c261bc1eaaf3
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2018-04-25 15:49:29 +0200

    Reformat Wikbase classes

--- app/Wikidata/Rdf.js
+++ app/Wikidata/Rdf.js
@@ -1,152 +1,105 @@
-var wikibase = wikibase || {};
-wikibase.queryService = wikibase.queryService || {};
-wikibase.queryService.ui = wikibase.queryService.ui || {};
-wikibase.queryService.ui.editor = wikibase.queryService.ui.editor || {};
-wikibase.queryService.ui.editor.hint = wikibase.queryService.ui.editor.hint || {};
+'use strict';
 
-( function( $, wikibase ) {
-	'use strict';
+const $ = require('jquery');
+const Wikibase = require('./Wikibase');
 
-	var MODULE = wikibase.queryService.ui.editor.hint;
+function getCurrentWord(line, position) {
+  let pos = position - 1;
 
-	/**
-	 * Code completion for Wikibase entities RDF prefixes in SPARQL completes SPARQL keywords and ?variables
-	 *
-	 * @class wikibase.queryService.ui.editor.hint.Rdf licence GNU GPL v2+
-	 *
-	 * @author Jonas Kress
-	 * @param {wikibase.queryService.api.Wikibase} api
-	 * @param {wikibase.queryService.RdfNamespaces} rdfNamespaces
-	 * @constructor
-	 */
-	var SELF = MODULE.Rdf = function( api, rdfNamespaces ) {
-		this._api = api;
-		this._rdfNamespaces = rdfNamespaces;
+  if (pos < 0) {
+    pos = 0;
+  }
 
-		if ( !this._api ) {
-			this._api = new wikibase.queryService.api.Wikibase();
-		}
+  while ('>'.indexOf(line.charAt(pos)) === -1) {
+    pos--;
+    if (pos < 0) {
+      break;
+    }
+  }
+  const left = pos + 1;
 
-		if ( !this._rdfNamespaces ) {
-			this._rdfNamespaces = wikibase.queryService.RdfNamespaces;
-		}
-	};
+  pos = position;
+  while ('<'.indexOf(line.charAt(pos)) === -1) {
+    pos++;
+    if (pos >= line.length) {
+      break;
+    }
+  }
+  const right = pos;
 
-	/**
-	 * @property {wikibase.queryService.RdfNamespaces}
-	 * @private
-	 */
-	SELF.prototype._rdfNamespaces = null;
+  const word = line.substring(left, right);
+  return {
+    word,
+    start: left,
+    end: right
+  };
+}
 
-	/**
-	 * @property {wikibase.queryService.api.Wikibase}
-	 * @private
-	 */
-	SELF.prototype._api = null;
+function getHintCompletion(lineNum, currentWord, list) {
+  const completion = {
+    list: []
+  };
+  completion.from = {
+    line: lineNum,
+    char: currentWord.start
+  };
+  completion.to = {
+    line: lineNum,
+    char: currentWord.end
+  };
+  completion.list = list;
 
-	/**
-	 * Get list of hints
-	 *
-	 * @return {jQuery.Promise} Returns the completion as promise ({list:[], from:, to:})
-	 */
-	SELF.prototype.getHint = function( editorContent, lineContent, lineNum, cursorPos ) {
-		var deferred = new $.Deferred(),
-			currentWord = this._getCurrentWord( lineContent, cursorPos ),
-			list,
-			term,
-			self = this;
-		if ( !currentWord.word.match( /^Q.*/ ) ) {
-			return deferred.reject().promise();
-		}
+  return completion;
+}
 
-		term = currentWord.word.trim().substr(1);
+function searchEntities(term, type) {
+  const entityList = [];
+  const deferred = $.Deferred();
 
-		if ( term.length === 0 ) { // empty search term
-			list = [ {
-				text: term,
-				displayText: 'Type to search for an entity'
-			} ];
-			return deferred.resolve( this._getHintCompletion( lineNum, currentWord, list ) )
-					.promise();
-		}
+  (new Wikibase()).searchEntities(term, type).done((data) => {
+    $.each(data.search, (key, value) => {
+      entityList.push({
+        className: 'wikibase-rdf-hint',
+        text: value.id,
+        displayText: value.label + ' (' + value.id + ') ' + value.description + '\n'
+      });
+    });
 
-			this._searchEntities( term, 'item').done(
-					function( list ) {
-						return deferred.resolve( self._getHintCompletion( lineNum, currentWord,
-								 list ) );
-					} );
+    deferred.resolve(entityList);
+  });
 
-		return deferred.promise();
-	};
+  return deferred.promise();
+}
 
+/**
+ * Get list of hints
+ * @return {jQuery.Promise} Returns the completion as promise ({list:[], from:, to:})
+ */
+function getHint(lineContent, lineNum, cursorPos) {
+  const deferred = new $.Deferred();
+  const currentWord = getCurrentWord(lineContent, cursorPos);
+  let list;
+  if (!currentWord.word.match(/^Q.*/)) {
+    return deferred.reject().promise();
+  }
 
-	SELF.prototype._getHintCompletion = function( lineNum, currentWord, list ) {
-		var completion = {
-			list: []
-		};
-		completion.from = {
-			line: lineNum,
-			char: currentWord.start
-		};
-		completion.to = {
-			line: lineNum,
-			char: currentWord.end
-		};
-		completion.list = list;
+  const term = currentWord.word.trim().substr(1);
 
-		return completion;
-	};
+  if (term.length === 0) { // empty search term
+    list = [ {
+      text: term,
+      displayText: 'Type to search for an entity'
+    } ];
+    return deferred.resolve(getHintCompletion(lineNum, currentWord, list))
+      .promise();
+  }
 
-	SELF.prototype._searchEntities = function( term, type ) {
-		var entityList = [],
-			deferred = $.Deferred();
+  searchEntities(term, 'item').done(
+    (list) => {
+      return deferred.resolve(getHintCompletion(lineNum, currentWord,
+        list));
+    });
 
-		this._api.searchEntities( term, type ).done( function( data ) {
-			$.each( data.search, function( key, value ) {
-				entityList.push( {
-					className: 'wikibase-rdf-hint',
-					text: value.id,
-					displayText: value.label + ' (' + value.id + ') ' + value.description + '\n'
-				} );
-			} );
+  return deferred.promise();
+}
 
-			deferred.resolve( entityList );
-		} );
-
-		return deferred.promise();
-	};
-
-	SELF.prototype._getCurrentWord = function( line, position ) {
-		var pos = position - 1,
-			wordSeparator = [ '>','<'];
-
-		if ( pos < 0 ) {
-			pos = 0;
-		}
-
-		while ( '>'.indexOf( line.charAt( pos ) ) === -1 ) {
-			pos--;
-			if ( pos < 0 ) {
-				break;
-			}
-		}
-		var left = pos + 1;
-
-		pos = position;
-		while ( '<'.indexOf( line.charAt( pos ) ) === -1 ) {
-			pos++;
-			if ( pos >= line.length ) {
-				break;
-			}
-		}
-		var right = pos;
-
-		var word = line.substring( left, right );
-		return {
-			word: word,
-			start: left,
-			end: right
-		};
-	};
-
-}( jQuery, wikibase ) );
