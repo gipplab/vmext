68740d777ea8baf230958a482f017bc0a804f1a4
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2018-05-03 14:32:53 +0200

    Update link to test data

--- testData.js
+++ testData.js
@@ -3,9 +3,9 @@
 const glob = require('glob');
 const fs = require('fs');
 const getFileAsString = n => fs.readFileSync(n,'utf-8');
-const mml = glob.sync('data/*.mml.xml', 'utf8')
+const mml = glob.sync('frontend/data/*.mml.xml', 'utf8')
   .map(getFileAsString);
-const sim = glob.sync('data/*sim.json', 'utf8')
+const sim = glob.sync('frontend/data/*sim.json', 'utf8')
   .map(getFileAsString);
 const locals = {
   'locals' : { mml, sim }
